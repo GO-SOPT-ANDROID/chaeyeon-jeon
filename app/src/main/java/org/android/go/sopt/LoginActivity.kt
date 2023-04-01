3caa8ddc4e57798188e9460c8923e612d3f9a12c
b1urrrr <b1urrr@naver.com>
2023-04-01 17:56:55 +0900

    [FEAT/#1] 버튼 클릭 리스너 실습

--- app/src/main/java/org/android/go/sopt/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/LoginActivity.kt
@@ -1,11 +1,22 @@
 package org.android.go.sopt
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import org.android.go.sopt.databinding.ActivityLoginBinding
+import org.android.go.sopt.util.binding.BindingActivity
+import org.android.go.sopt.util.extension.showSnackbar
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        setLoginBtnClickListener()
+    }
+
+    private fun setLoginBtnClickListener() {
+        binding.btnLoginLogin.setOnClickListener {
+            this.showSnackbar(binding.root, "click login button")
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
