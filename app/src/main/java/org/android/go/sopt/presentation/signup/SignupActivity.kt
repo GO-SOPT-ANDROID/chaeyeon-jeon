4ce322f88c452c9f44883ac7e1746170c2aae7c1
b1urrrr <b1urrr@naver.com>
2023-04-04 02:45:16 +0900

    [UI/#1] 회원가입 버튼 추가

--- app/src/main/java/org/android/go/sopt/presentation/signup/SignupActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signup/SignupActivity.kt
@@ -1,11 +1,11 @@
-package org.android.go.sopt.presentation.main
+package org.android.go.sopt.presentation.signup
 
 import android.os.Bundle
 import org.android.go.sopt.R
-import org.android.go.sopt.databinding.ActivityMainBinding
+import org.android.go.sopt.databinding.ActivitySignupBinding
 import org.android.go.sopt.util.binding.BindingActivity
 
-class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+class SignupActivity : BindingActivity<ActivitySignupBinding>(R.layout.activity_signup) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
