b4548fa1c0c5e7d59ab83110e16dc99d65a75a1c
b1urrrr <b1urrr@naver.com>
2023-04-04 03:33:13 +0900

    [UI/#1] 회원가입 레이아웃 구현

--- app/src/main/java/org/android/go/sopt/presentation/login/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/login/LoginActivity.kt
@@ -4,20 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import org.android.go.sopt.R
 import org.android.go.sopt.databinding.ActivityLoginBinding
-import org.android.go.sopt.presentation.main.MainActivity
+import org.android.go.sopt.presentation.signup.SignupActivity
 import org.android.go.sopt.util.binding.BindingActivity
+import org.android.go.sopt.util.extension.hideKeyboard
 import org.android.go.sopt.util.extension.setOnSingleClickListener
 
 class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setLoginBtnClickListener()
+        initLayout()
+        initLoginBtnClickListener()
     }
 
-    private fun setLoginBtnClickListener() {
+    private fun initLayout() {
+        binding.layoutLogin.setOnSingleClickListener { hideKeyboard() }
+    }
+
+    private fun initLoginBtnClickListener() {
         binding.btnLoginLogin.setOnSingleClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, SignupActivity::class.java))
         }
     }
 }
--- app/src/main/java/org/android/go/sopt/presentation/signup/SignupActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signup/SignupActivity.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import org.android.go.sopt.R
 import org.android.go.sopt.databinding.ActivitySignupBinding
 import org.android.go.sopt.util.binding.BindingActivity
+import org.android.go.sopt.util.extension.hideKeyboard
+import org.android.go.sopt.util.extension.setOnSingleClickListener
 
 class SignupActivity : BindingActivity<ActivitySignupBinding>(R.layout.activity_signup) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initLayout()
+    }
+
+    private fun initLayout() {
+        binding.layoutSignup.setOnSingleClickListener { hideKeyboard() }
     }
 }
