8378c126af43c1224591684779fcfd5179f5b9dd
b1urrrr <b1urrr@naver.com>
2023-05-04 05:21:25 +0900

    [ADD/#6] 아이콘 추가 및 스플래시 화면 설정

--- app/src/main/java/org/android/go/sopt/presentation/login/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/login/LoginActivity.kt
@@ -8,8 +8,8 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.android.go.sopt.R
-import org.android.go.sopt.domain.model.User
 import org.android.go.sopt.databinding.ActivityLoginBinding
+import org.android.go.sopt.domain.model.User
 import org.android.go.sopt.presentation.main.MainActivity
 import org.android.go.sopt.presentation.signup.SignupActivity
 import org.android.go.sopt.util.UiState.Failure
