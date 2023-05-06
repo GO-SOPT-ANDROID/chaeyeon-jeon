59988f890d200bd3c1c8dc4da145b2b0cd3fb3b2
b1urrrr <b1urrr@naver.com>
2023-05-06 15:13:45 +0900

    [FEAT/#8] Gallery ViewPager 구현

--- app/src/main/java/org/android/go/sopt/presentation/main/gallery/GalleryFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/main/gallery/GalleryFragment.kt
@@ -7,8 +7,30 @@ import org.android.go.sopt.databinding.FragmentGalleryBinding
 import org.android.go.sopt.util.binding.BindingFragment
 
 class GalleryFragment : BindingFragment<FragmentGalleryBinding>(R.layout.fragment_gallery) {
+    private var imageAdapter: ImageAdapter? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initImageAdapter()
+    }
+
+    private fun initImageAdapter() {
+        imageAdapter = ImageAdapter()
+        binding.vpGallery.adapter = imageAdapter
+        imageAdapter?.submitList(
+            listOf(
+                R.drawable.img_main_profile,
+                R.drawable.img_main_profile,
+                R.drawable.img_main_profile,
+            ),
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+        imageAdapter = null
     }
 
     companion object {
