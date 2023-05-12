2e8e9e5b6d5a4588eb26a451705e52afda26c028
b1urrrr <b1urrr@naver.com>
2023-05-12 19:32:04 +0900

    [ADD/#8] placeholder, error, fallback 이미지 추가

--- app/src/main/java/org/android/go/sopt/util/binding/BindingAdapter.kt
+++ app/src/main/java/org/android/go/sopt/util/binding/BindingAdapter.kt
@@ -4,13 +4,16 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.RoundedCornersTransformation
+import org.android.go.sopt.R
 
 object BindingAdapter {
     @JvmStatic
     @BindingAdapter("setRoundedCornersImage")
     fun ImageView.setRoundedCornersImage(img: String?) {
         load(img) {
-            // TODO: placeholder & load error 이미지 추가
+            placeholder(R.mipmap.ic_go_sopt)
+            error(R.drawable.ic_image_not_supported)
+            fallback(R.drawable.ic_image_not_supported)
             transformations(RoundedCornersTransformation(50f))
         }
     }
